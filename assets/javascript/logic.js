ee2c53e3b20cae5e4b02d0071ab2acfc50c51600
Ricardo Paulin <rpaulin@ricardos-mbp.attlocal.net>
2017-04-10 11:02:15 -0500

    added css and mechanism to pause play gifs

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,13 +8,14 @@ var initialButtons = ["cat", "dog", "guinea pig", "elephant", "snow leopard", "m
 
 var createButton = function(text){
     var new_button = $("<button>");
-    new_button.addClass("GifBtn");
+    new_button.addClass("GifBtn btn btn-primary");
     new_button.text(text);
     $(".buttonContainer").append(new_button);
 }
 
 for (var x = 0; x < initialButtons.length; x++) {
     createButton(initialButtons[x]);
+    $(".GifBtn").attr("status","unpressed");
 }
 
 $("#searchButton").on("click", function (event) {
@@ -27,31 +28,90 @@ $("#searchButton").on("click", function (event) {
 
 $(document).on("click", ".GifBtn", function () {
 
+    if($(this).attr("status") === "unpressed") {
+
+        var apiURL = "https://api.giphy.com/v1/gifs/search?limit=10&rating=pg&q=";
+
+        var apiKey = "&api_key=dc6zaTOxFJmzC";
+
+        var buttonText = $(this).attr("class", "GifBtn").text();
+
+        $(this).attr("status", "pressed");
+
+        $.ajax({
+            method: "GET",
+            url: apiURL + buttonText + apiKey
+        }).done(function (response) {
+
+            for (var i = 0; i < 10; i++) {
+
+                var gifDiv = $("<div>");
+
+                var img_tag = $("<img>");
+
+                var rating_p = $("<p>");
+
+                img_tag.attr("src", response.data[i].images.fixed_height_still.url);
+
+                gifDiv.addClass("GIF_Div");
+
+                img_tag.addClass("GIF");
+
+                img_tag.attr("state", "still");
+
+                img_tag.attr("value", buttonText);
+
+                img_tag.attr("number", [i]);
+
+                rating_p.text("GIF Rating: " + response.data[i].rating);
+
+                gifDiv.append(img_tag);
+                gifDiv.append(rating_p);
+
+                $("#gifContainer").append(gifDiv);
+
+            }
+
+        })
+    }
+});
+
+$(document).on("click", ".GIF", function () {
+
     var apiURL = "https://api.giphy.com/v1/gifs/search?limit=10&rating=pg&q=";
 
     var apiKey = "&api_key=dc6zaTOxFJmzC";
 
-    $.ajax({
-        method: "GET",
-        url: apiURL + $(this).text() + apiKey
-    }).done(function (response) {
+    var thisBtn = $(this).attr("class", "GIF");
 
-        for (var i = 0; i < 10; i++) {
+    if (thisBtn.attr("state") === "still") {
+        $.ajax({
+            method: "GET",
+            url: apiURL + thisBtn.attr("value") + apiKey
+        }).done(function (response) {
+            thisBtn.attr("src", response.data[thisBtn.attr("number")].images.fixed_height.url);
+            thisBtn.attr("state", "animated");
+        })
+    }
 
-            var img_tag = $("<img>");
+    else {
+        $.ajax({
+            method: "GET",
+            url: apiURL + thisBtn.attr("value") + apiKey
+        }).done(function (response) {
+            thisBtn.attr("src", response.data[thisBtn.attr("number")].images.fixed_height_still.url);
+            thisBtn.attr("state", "still");
+        })
+    }
+});
 
-            var rating_p = $("<p>");
+$(".clean").on("click",function () {
 
-            img_tag.attr("src", response.data[i].images.fixed_height.url);
+    $("#gifContainer").empty();
 
-            rating_p.html(response.data[i].rating);
-
-            $("#gifContainer").append(img_tag);
-
-            $("#gifContainer").append(rating_p);
-
-        }
-
-    });
+    $(".GifBtn").addClass("btn btn-primary");
 
 })
+
+
+
